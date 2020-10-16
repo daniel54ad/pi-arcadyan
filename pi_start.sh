9b431f3cacaa22a3b82928703e199c37b2f80e28
yubintw <yubin.cs05g@nctu.edu.tw>
2020-10-16 06:39:35 +0000

    Multi-wan routing setting

--- pi_start.sh
+++ pi_start.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-# usage: ./pi_start.sh WAN_INTERFACE
+# usage: ./pi_start.sh 
 
-if [ $# -lt 1 ];  then
-    echo "usage: ./pi_start.sh WAN_INTERFACE"
+# wan interface: eth0, eth1
+
+if [ $# -lt 0 ];  then
+    echo "usage: ./pi_start.sh"
     exit 0
 fi
 
-WAN_INTERFACE=$1
-
-# link
-sudo ip link set eth1 up && sudo ovs-vsctl add-port ovsbr0 eth1
+# link of device-connect
 sudo ip link set eth2 up && sudo ovs-vsctl add-port ovsbr0 eth2
 
 sudo ovs-vsctl add-port ovsbr0 dhcper -- set interface dhcper type=internal
@@ -19,6 +18,16 @@ sudo ip link set dhcper up
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo service dnsmasq start
 
-# NAT
-sudo iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -o ${WAN_INTERFACE} -j MASQUERADE
+# mark by DCSP
+sudo iptables -t mangle -A PREROUTING -m dscp --dscp 1 -j MARK --set-mark 1
+sudo iptables -t mangle -A PREROUTING -m dscp --dscp 2 -j MARK --set-mark 2
 
+# ip rule
+sudo ip rule add fwmark 1 table table1 priority 100
+sudo ip rule add fwmark 2 table table2 priority 101
+
+# NAT
+sudo iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -o eth0 -j MASQUERADE
+sudo iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -o eth1 -j MASQUERADE
+
+sudo sysctl -w net.ipv4.conf.all.rp_filter=2
--- route-setting.sh
+++ route-setting.sh
@@ -8,30 +8,41 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-default_GW=192.168.50.1
 
-P1_NET=192.168.50.0/24
 IF1=eth0
-IP1=192.168.50.33
-T1=table0
-GW1=192.168.50.1
+T1=table1
 
-P2_NET=192.168.0.0/24
-IF2=wlan0
-IP2=192.168.0.15
-T2=table1
-GW2=192.168.0.1
+IF2=eth1
+T2=table2
+
+IP1=`ip addr show ${IF1} | awk '/inet / {split($2,a,"/"); print a[1]}'`
+IP2=`ip addr show ${IF2} | awk '/inet / {split($2,a,"/"); print a[1]}'`
+
+default_GW=${IP1}
+
+GW1=`ip route show default dev ${IF1}| awk '{print $3}'`
+GW2=`ip route show default dev ${IF2}| awk '{print $3}'`
+
+P1_NET=`ip route show dev ${IF1} | awk '/kernel/ {print $1}'`
+P2_NET=`ip route show dev ${IF2} | awk '/kernel/ {print $1}'`
+
+echo "IP1 = $IP1"
+echo "IP2 = $IP2"
+echo "Gateway1 = $GW1"
+echo "Gateway2 = $GW2"
+echo "P1_NET = $P1_NET"
+echo "P2_NET = $P2_NET"
 
 set -x
 ip route add $P1_NET dev $IF1 src $IP1 table $T1
-ip route add default via $GW1 table $T1
+ip route add default via $GW1 dev $IF1 table $T1
 ip route add $P2_NET dev $IF2 src $IP2 table $T2
-ip route add default via $GW2 table $T2
+ip route add default via $GW2 dev $IF2 table $T2
 
 ip route add $P1_NET dev $IF1 src $IP1
 ip route add $P2_NET dev $IF2 src $IP2
 
 ip route add default via $default_GW
 
-ip rule add from $IP1 table $T1
-ip rule add from $IP2 table $T2
+ip rule add from $IP1 table $T1 priority 1000
+ip rule add from $IP2 table $T2 priority 1001
