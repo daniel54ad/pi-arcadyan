0a44204b397174723383fc2e07c583b0fcdbb0ba
Ubuntu <ubuntu@ubuntu.lan>
2021-05-30 10:17:48 +0000

    add clean action

--- clean_test.sh
+++ clean_test.sh
@@ -4,3 +4,6 @@ sudo ovs-vsctl del-br ovsbr0
 sudo ip l del vx11
 sudo iptables -F -t nat
 sudo iptables -F -t mangle
+
+sudo /usr/local/share/openvswitch/scripts/ovs-ctl stop
+sudo /usr/local/share/openvswitch/scripts/ovs-ctl start
